8d5a62b27b8fdf213a3be658b788a30302f0d4f8
Julienmj <julienmugisha1@gmail.com>
2026-08-03 12:50:48 +0200

    adding py exercise files for today

---
+++ week1_mon.py
@@ -0,0 +1,9 @@
+# declaring variable (varible is just a name pointing to a storage in the ram )
+
+name = "Mugisha Julien"
+print (name)
+
+# intput 
+
+age = int(input ("enter your age : "))
+print (age)
---
+++ week1_mon_ex2.py
@@ -0,0 +1,9 @@
+""" 
+dob = input ("enter date of birth (yyyy-mm-dd) :")
+age = 2026 - int (dob.split("-")[0]) 
+print ("your age is ", age)
+
+"""
+name = "python"
+
+print (name[2])

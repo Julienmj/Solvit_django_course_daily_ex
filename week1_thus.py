b02e77d108454d6d8fc790124a7dbc1a01b8b5f9
Julienmj <julienmugisha1@gmail.com>
2026-08-06 16:52:21 +0200

    adding thusday lesson

---
+++ week1_thus.py
@@ -0,0 +1,34 @@
+# c = 1
+# while c <=5 :
+#     print (c, c<=5)
+#     c +=1
+
+
+mg =  []
+student = {}
+choice = "y"
+
+
+while choice == "y" :
+
+    student["name"] = input("enter name:")
+
+    choice = input("new user?" )
+
+    mg.append(student)
+
+    
+        
+
+    print(mg)
+
+
+
+
+
+
+
+
+ 
+
+
